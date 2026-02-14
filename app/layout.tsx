93209ea79a6931058b932b7e90aa944b10be3b07
RestoReview <restoreview.connect@gmail.com>
2026-02-14 12:51:38 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body style={{ margin: 0, padding: 0 }}>{children}</body>
+      <body style={{ margin: 0, padding: 0, fontFamily: 'sans-serif' }}>
+        {children}
+      </body>
     </html>
   );
 }
